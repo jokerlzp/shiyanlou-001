4a08bf95490e1e6d73fe219d8f17b9707c3b60d0
killerlzp <516261303@qq.com>
2018-04-03 14:47:24 +0800

    module prc

---
+++ bars.py
@@ -0,0 +1,20 @@
+def starbar(num):
+    """打印 * 分割线
+
+    :arg num: 线长
+    """
+    print('*' * num)
+
+def hashbar(num):
+    """打印 # 分割线
+
+    :arg num: 线长
+    """
+    print('#' * num)
+
+def simplebar(num):
+    """打印 - 分割线
+
+    :arg num: 线长
+    """
+    print('-' * num)
